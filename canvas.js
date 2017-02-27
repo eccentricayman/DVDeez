3036f1726fbefb76b232e19eab71f4b551b56e6f
eccentricayman <aahmed3@stuy.edu>
2017-02-27 03:27:33 -0500

    I FROGOT THE DRAW() LOL

--- canvas.js
+++ canvas.js
@@ -1,66 +1,90 @@
 //model for HTML5 canvas-based animation
 
 //access canvas and buttons via DOM
-var c = document.getElementById("playground");
-var stopButton = document.getElementById( "stop" );
+var c = document.getElementById("mainCanvas");
 
 //prepare to interact with canvas in 2D
 var ctx = c.getContext("2d");
 
 //set fill color to lello
-ctx.fillStyle = "#ffff00";
-
+ctx.fillStyle = "#222";
 
 var requestID;
 
-
-var clear = function(e) {
-    e.preventDefault();
-    ctx.clearRect(0, 0, 500, 500);
-};
-
-
-//wrapper function will allow inner function to keep track of
-// its own complement of local variables (radius, xcor...)
-var anime = function() {
-	
-    window.cancelAnimationFrame( requestID );
-	
-    console.log(requestID);
-
-    //init params for drawing dot
-    var radius = 50;
-    var xcor = c.width / 2;
-    
-    //Q: what happens w/ & w/o next line?
-    //window.cancelAnimationFrame( requestID );
-
-    var drawDot = function() {
-	    console.log( requestID );
-
-	    ctx.clearRect( 0, 0, c.width, c.height );
-	    
-	    ctx.beginPath();
-	    ctx.arc( xcor, 150, radius, 0, 2 * Math.PI );
-	    ctx.stroke();
-	    ctx.fill();
-
-	    xcor++;
-
-	    requestID = window.requestAnimationFrame( drawDot );
-    };
-    drawDot();
-};
-
+var clear = function() {
+    ctx.clearRect(0, 0, c.width, c.height)
+}
 
 var stopIt = function() {
     console.log( requestID );
     window.cancelAnimationFrame( requestID );
 };
 
+var circleAnim = function() {
+    stopIt();
 
-//tie click-on-canvas to anime function
-c.addEventListener( "click", anime );
+    var x = c.width / 2;
+    var y = c.height / 2;
+    var v = 1;
+    var r = 1;
+
+    var draw = function() {
+        clear();
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, 2 * Math.PI);
+        ctx.fill();
+        requestID = window.requestAnimationFrame(draw);
+        if (r > 250 || r == 0) {
+            v *= -1;
+        }
+        r += v;
+    }
+    draw();
+};
+
+var dvdAnim = function() {
+    stopIt();
+
+    var dw = new Image();
+    dw.src = "jadw.png";
+
+    var vx = Math.random() + 1;
+    var vy = Math.random() + 1;
+
+    var h = 100;
+    var w =  100;
+
+    //x
+    var max_w = c.width - w;
+    //y
+    var max_h = c.height - h;
+
+    //random pos
+    var x = max_w * Math.random();
+    var y = max_h * Math.random();
+
+    var draw = function() {
+        clear();
+        ctx.drawImage(dw, x, y);
+        requestID = window.requestAnimationFrame(draw);
+        if (x > max_w || x < 0) {
+            vx *= -1;
+        }
+        if (y > max_h || y < 0) {
+            vy *= -1;
+        }
+        x += vx;
+        y += vy;
+    };
+    draw();
+};
+
+var circle = document.getElementById("circle");
+circle.addEventListener("click", circleAnim);
+
+var dvd = document.getElementById("dvd");
+dvd.addEventListener("click", dvdAnim);
 
 //ideally, clicking stop will make the animation stop
+var stopButton = document.getElementById( "stop" );
 stopButton.addEventListener( "click",  stopIt );
